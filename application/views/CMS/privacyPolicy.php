d69ca0d32e5315f4ecb913d4b146aa18ee43f134
dev <dev@develop.com>
2019-11-27 23:49:01 +0530

    changes

---
+++ application/views/CMS/privacyPolicy.php
@@ -0,0 +1,24 @@
+<h2>Songs of Jesus Ministry App - Privacy Policy</h2>
+<pre>
+Data Protection Policy (Part A.) and User Consent (Part B.)
+
+Regarding the Use of Songs of Jesus Ministry App
+A. Information on Data Protection
+Songs of Jesus Ministry App,
+
+
+1.Songs of Jesus Ministry App will respect applicable data protection laws.
+
+2.Songs of Jesus Ministry App will only collect, process and use personal data in connection with its services and will not provide third parties with such data unless the User has provided his/her consent or Songs of Jesus Ministry App is permitted by law or obligated to do so in order to comply with any legal obligation.
+
+3. Songs of Jesus Ministry App contains links to websites of third parties and cannot influence the way such third parties are collecting, processing and using personal data of visitors to these sites. Therefore, Songs of Jesus Ministry App declines any responsibility and liability for such third parties’ acts.
+
+4.Songs of Jesus Ministry Appis dedicated to the protection of the personal data of its Users and has therefore taken appropriate measures for the protection of this data. However, Songs of Jesus Ministry App declines any responsibility and liability for the disclosure of User data caused by defective data transfers and/or illegal access by third parties.
+
+5.Songs of Jesus Ministry Appcollects only the domain name, but not the e-mail address of visitors to its Web page, the e-mail addresses of those who post messages to the bulletin board, the e-mail addresses of those who communicate with us via e-mail, the e-mail addresses of those who make postings to our chat areas, aggregate information on what pages consumers access or visit, User-specific information on what pages consumers access or visit, information volunteered by the user, such as survey information and/or site registrations, name and address, telephone number, fax number.
+
+Cookies and Log-Files
+I agree that Songs of Jesus Ministry Appuses cookies as follows:
+Songs of Jesus Ministry Appuses cookies to store visitors’ preferences, record session information, record user-specific information on what pages users access or visit, record past activity at a site in order to provide better service when visitors return to our site, customize Web page content based on visitors' browser type or other information that the visitor sends.
+Cookies are text files that are either temporarily stored in the random access memory or on the hard drive of your computer. They generally serve for offering the User the services of Songs of Jesus Ministry App with the User’s personal settings in each new session. Cookies are partly stored permanently on the User’s hard drive. The User may prevent this by setting his/her browser accordingly.
+</pre>
