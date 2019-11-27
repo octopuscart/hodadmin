1b5de82c30530e9a6ba00b0bd7162c3e3edfeca6
dev <dev@develop.com>
2019-11-27 23:55:42 +0530

    changes

---
+++ application/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+       
+    }
+
+    public function privacyPolicy() {
+        $this->load->view('CMS/privacyPolicy');
+    }
+}
+?>
